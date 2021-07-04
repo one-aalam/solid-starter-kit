a944a3da38ddf0cdd77f2046eb985284d4a2630a
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-07-05 00:28:55 +0530

    Add animation to alert component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,7 +17,7 @@ const Home: Component = () => (
         <div className="mt-5 flex gap-4">
             <button class="bg-blue-700 text-white px-2 py-1 rounded-md" onClick={() => handleAlert({ type: 'default' , text: 'Wohoo!'})}>Show Alert</button>
             <button class="bg-green-700 text-white px-2 py-1 rounded-md" onClick={() => handleAlert({ type: 'success' , text: 'Yesssss!'})}>Show Success</button>
-            <button class="bg-green-700 text-white px-2 py-1 rounded-md" onClick={() => handleAlert({ type: 'error' , text: 'Ewwwww!'})}>Show Error</button>
+            <button class="bg-red-700 text-white px-2 py-1 rounded-md" onClick={() => handleAlert({ type: 'error' , text: 'Ewwwww!'})}>Show Error</button>
         </div>
     </DefaultLayout>
 );
